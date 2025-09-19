7d10c928c8e1ccdcf2ab5aae66d0885b40536e26
Sean Sellitto <sean.sellitto@gmail.com>
2025-09-18 20:19:47 -0400

    fetch jokes from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,30 @@
+import { useState, useEffect } from "react";
+
+const url = "https://icanhazdadjoke.com";
+
 export default function App() {
-  return <h1>Hello React (minimal setup)!</h1>;
+  const [joke, setJoke] = useState("");
+
+  async function getJokes() {
+    const response = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    const data = await response.json();
+    setJoke(data.joke);
+  }
+
+  useEffect(() => {
+    getJokes();
+  }, []);
+
+  return (
+    <>
+      <h1>Dad Joke Generator</h1>
+      <p>{joke}</p>
+      <button onClick={getJokes}>Get New Joke</button>
+    </>
+  );
 }
